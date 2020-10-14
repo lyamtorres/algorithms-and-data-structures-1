fdc10dce64953a5650797576360c3409f2fc8dea
lyamtorres <lyamtorres@hotmail.com>
2020-10-14 12:37:34 +0200

    Update td2 ex1

--- td2/td2-ex1.cpp
+++ td2/td2-ex1.cpp
@@ -1,4 +1,4 @@
-/* 1 - Pointeurs  - Lyam Torres
+/* Pointeurs  - Lyam Torres
  */
 
 #include <iostream>
@@ -7,40 +7,90 @@ using namespace std;
 
 struct deuxSymb {
     char symb;
-    char adrLettre;
+    char *adrLettre;
 };
 
-void saisir();
 
-deuxSymb saisir2();
-
+// Echange les valeurs entre symb et adrLettre avec une procédure
 void permuter(deuxSymb *ds);
 
-deuxSymb permuter2 (deuxSymb *ds);
+// Echange les valeurs entre symb et adrLettre en utilisant une fonction
+deuxSymb *permuterBis(deuxSymb *ds);
+
+/* void saisir(); */
+
+// Saisit un deuxSymb auprès de l'utilisateur en utilisant une fonction
+deuxSymb *saisirBis();
+
 
 // Fonction principale
 int main() {
-    char *pcar;
     deuxSymb *duo;
-    deuxSymb *clone;
-    char tmp;
-    
-/*
+    deuxSymb *nouvelDuo;
+
+/*  
+        // Test pour la procédure permuter
 
     duo = new(deuxSymb);
-    *duo = saisir2();
+    duo->symb = 'G';
+    duo->adrLettre = new(char);
+    *duo->adrLettre = 'Y';
+    permuter(duo);
+
     cout << "duo->symb: " << duo->symb << endl;
     cout << "duo->adrLettre: " << duo->adrLettre << endl;
 
+    delete duo->adrLettre;
+    duo->adrLettre = nullptr;
+    delete duo; 
+    duo = nullptr;
+
+    return 0;
+
+
+        // Test pour la procédure permuterBis
+
+    duo = new(deuxSymb);
+    duo->symb = 'A';
+    duo->adrLettre = new(char);
+    *duo->adrLettre = 'B';
+    nouvelDuo = permuterBis(duo);
+
+    cout << "duo->symb: " << duo->symb << endl;
+    cout << "duo->adrLettre: " << duo->adrLettre << endl;
+    cout << "" << endl;
+    cout << "nouvelDuo->symb: " << nouvelDuo->symb << endl;
+    cout << "nouvelDuo->adrLettre: " << nouvelDuo->adrLettre << endl;
+
+    delete duo->adrLettre;
+    delete duo;  
+    delete nouvelDuo->adrLettre;
+    delete nouvelDuo;
+
+    return 0;
 */
-    duo = new(deuxSymb); // Allocation dynamique
-    duo->symb = 'G';
-    duo->adrLettre = 'Y';
-    clone = duo;
-    cout << clone->symb << endl;
-    cout << clone->adrLettre << endl;
 
 }
+
+
+void permuter(deuxSymb *ds) {
+    char tmp = '';
+    
+    tmp = ds->symb;
+    ds->symb = *ds->adrLettre;
+    *ds->adrLettre = tmp;
+}
+
+deuxSymb *permuterBis(deuxSymb *ds) {
+    deuxSymb *nouvDs = new deuxSymb;
+
+    nouvDs->symb = *ds->adrLettre; 
+    nouvDs->adrLettre = new char;
+    *nouvDs->adrLettre = ds->symb;
+    return nouvDs;
+}
+
+/*
 
 void saisir() {
     deuxSymb *ds;
@@ -54,32 +104,17 @@ void saisir() {
     cout << "ds->adrLettre: " << ds->adrLettre << endl;
 }
 
-deuxSymb saisir2() {
+
+deuxSymb *saisirBis() {
     deuxSymb *ds;
 
     ds = new(deuxSymb);
     cout << "Saisir le premier symbole: " << endl;
     cin >> ds->symb;
+    ds->adrLettre = new(char);
     cout << "Saisir le deuxieme symbole: " << endl;
     cin >> ds->adrLettre;
-    return *ds;
+    return ds;
 }
 
-void permuter(deuxSymb *ds) {
-    char tmp;
-    
-    tmp = ds->symb;
-    ds->symb = ds->adrLettre;
-    ds->adrLettre = tmp;
-    cout << "ds->symb: " << ds->symb << endl;
-    cout << "ds->adrLettre: " << ds->adrLettre << endl;
-}
-
-deuxSymb permuter2(deuxSymb *ds) {
-    char tmp;
-    
-    tmp = ds->symb;
-    ds->symb = ds->adrLettre;
-    ds->adrLettre = tmp;
-    return *ds; 
-}
+*/
