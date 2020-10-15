71931b47b5ef7d05d28524b0245b7d2c6989195b
lyamtorres <lyamtorres@hotmail.com>
2020-10-15 10:52:08 +0200

    Complete td2 ex1

--- td2/td2-ex1.cpp
+++ td2/td2-ex1.cpp
@@ -1,5 +1,6 @@
-/* Pointeurs  - Lyam Torres
- */
+/* 
+            1 - Pointeurs
+*/
 
 #include <iostream>
 
@@ -17,10 +18,14 @@ void permuter(deuxSymb *ds);
 // Echange les valeurs entre symb et adrLettre en utilisant une fonction
 deuxSymb *permuterBis(deuxSymb *ds);
 
-/* void saisir(); */
+// Saisit un deuxSymb auprès de l'utilisateur avec une procédure
+void saisir(deuxSymb *ds);
 
 // Saisit un deuxSymb auprès de l'utilisateur en utilisant une fonction
 deuxSymb *saisirBis();
+
+// Fait une copie d'un deuxSymb dans un autre
+void cloner(deuxSymb *ds, deuxSymb *copieDs);
 
 
 // Fonction principale
@@ -68,13 +73,60 @@ int main() {
     delete nouvelDuo;
 
     return 0;
+
+
+                // Test pour la procédure saisir
+
+    duo = new deuxSymb;
+    duo->adrLettre = new char;
+    saisir(duo);
+
+    cout << "duo->symb: " << duo->symb << endl;
+    cout << "duo->adrLettre: " << duo->adrLettre << endl;
+    
+    delete duo->adrLettre;
+    delete duo;
+
+    return 0;
+
+
+            // Test pour la fonction saisirBis
+
+    duo = saisirBis();
+
+    cout << "duo->symb: " << duo->symb << endl;
+    cout << "duo->adrLettre: " << duo->adrLettre << endl;
+    
+    delete duo->adrLettre;
+    delete duo;
+
+    return 0;
 */
 
+            // Test pour la fonction cloner
+
+    duo = new deuxSymb;
+    duo->symb = 'A';
+    duo->adrLettre = new char;
+    *duo->adrLettre = 'B';
+
+    nouvelDuo = new deuxSymb;
+    nouvelDuo->adrLettre = new char;
+    
+    cloner(duo, nouvelDuo);
+
+    cout << "nouvelDuo->symb: " << nouvelDuo->symb << endl;
+    cout << "nouvelDuo->adrLettre: " << nouvelDuo->adrLettre << endl;
+
+    delete duo->adrLettre;
+    delete duo;  
+    delete nouvelDuo->adrLettre;
+    delete nouvelDuo;
 }
 
 
 void permuter(deuxSymb *ds) {
-    char tmp = '';
+    char tmp;
     
     tmp = ds->symb;
     ds->symb = *ds->adrLettre;
@@ -90,31 +142,28 @@ deuxSymb *permuterBis(deuxSymb *ds) {
     return nouvDs;
 }
 
-/*
-
-void saisir() {
-    deuxSymb *ds;
-
-    ds = new(deuxSymb);
-    cout << "Saisir le premier symbole: " << endl;
+void saisir(deuxSymb *ds) { // Existe t-il une méthode pour passer faire l'allocation directement dans la procédure ?
+    cout << "Saisir le premier symbole: ";
     cin >> ds->symb;
-    cout << "Saisir le deuxieme symbole: " << endl;
+    cout << "Saisir le deuxieme symbole: ";
     cin >> ds->adrLettre;
-    cout << "ds->symb: " << ds->symb << endl;
-    cout << "ds->adrLettre: " << ds->adrLettre << endl;
 }
 
 
 deuxSymb *saisirBis() {
     deuxSymb *ds;
 
-    ds = new(deuxSymb);
+    ds = new deuxSymb ;
     cout << "Saisir le premier symbole: " << endl;
     cin >> ds->symb;
-    ds->adrLettre = new(char);
+    ds->adrLettre = new char;
     cout << "Saisir le deuxieme symbole: " << endl;
-    cin >> ds->adrLettre;
+    cin >> *ds->adrLettre;
     return ds;
 }
 
-*/
+void cloner(deuxSymb *ds, deuxSymb *copieDs) {
+    copieDs->symb = ds->symb;
+    *copieDs->adrLettre = *ds->adrLettre;
+}
+
