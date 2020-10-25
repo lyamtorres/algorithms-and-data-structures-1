05137cb786dcc6d1b56897fc9cbbf595cb19799c
lyamtorres <lyamtorres@hotmail.com>
2020-10-25 13:15:01 +0100

    Add td1 and td2 folders

---
+++ travaux-diriges/td1/3-combien.cpp
@@ -0,0 +1,22 @@
+/* 
+            3 - Combien
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int nb, toto, cpt;
+
+    cout << "Donnez la valeur de nb: ";
+    cin >> nb; // Tester pour nb = 1, 2 et 3
+    toto = 0; cpt = 0;
+    while(cpt <= nb) {
+        toto = toto + cpt;
+        cpt = cpt + 1;
+    }
+    cout << "nb = "<< toto << endl;
+}
+
+// Coût théorique = 5(nb + 2)
--- travaux-diriges/td1/4-dates.cpp
+++ travaux-diriges/td1/4-dates.cpp
@@ -3,9 +3,39 @@
 */
 
 #include <iostream>
-#include "main.h"
 
 using namespace std;
+
+struct Instant {
+    int heure;
+    int minutes;
+    int secondes;
+};
+
+/*
+            Prototypes des fonctions
+*/
+
+// Retourne un instant saisie par l'utilisateur
+Instant saisirInstant();
+
+// Affiche la somme de deux instants differents en secondes
+void convertir(Instant t1, Instant t2);
+
+// Retourne vrai si val1 < val2 et faux dans le cas contraire
+bool comparer(int val1, int val2); 
+
+// Echange l'ordre de val1 et val2 dans l'algorithme
+void permuter(int &t1, int &t2, int &tmp);
+
+// Affiche un instant dans un format hh:mm:ss
+void afficher(Instant t);
+
+// Avance d'une seconde un instant m 
+void avancerUneSeconde(Instant &t);
+
+// Avance d'une seconde un instant m en retournant un nouvel Instant
+Instant avancerUneSecondeBis(Instant t);
 
 
 int main() {
